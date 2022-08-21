65c1188ba816d4de12d1f3ad5895536395056d62
LellaHemasriSai <cs20b020@iittp.ac.in>
2022-08-21 19:20:50 +0530

    App

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import './App.css'
-import Home from './Faculty/Home/home'
-import Register from './register/register';
-import LoginFac from './login2/login2_fac';
-import Login1 from './login1/login1';
-import Link from "./Link";
+import React from "react";
+import "./App.css";
+import Home from "./Faculty/Home/home";
+import Register from "./register/register";
+import LoginFac from "./login2/login2_fac";
+import Login1 from "./login1/login1";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     <Link />
-=======
       <Register />
->>>>>>> 816703a121d2fcc17e217a876063898c80063b05
     </div>
   );
 }
 
-export default App;    
-
-
-
-
-
+export default App;
