6e9aba68213009d99980cc11c64310848a86e74f
Preethi Varsha <82671452+PreethiVarshaM@users.noreply.github.com>
2022-08-21 17:39:12 +0530

    changed login1

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -3,10 +3,12 @@ import './App.css'
 import Home from './Faculty/Home/home'
 import Register from './register/register';
 import LoginFac from './login2/login2_fac';
+import Login1 from './login1/login1';
+
 function App() {
   return (
     <div className="App">
-      <LoginFac />
+      <Login1 />
     </div>
   );
 }
--- myapp/src/components/login1/login1.js
+++ myapp/src/components/login1/login1.js
@@ -1,14 +1,36 @@
-function onClickStudent()
-{
-    window.location.href="../login2/login2_stu.html";
+import React from "react";
+import "../login1/login1.css";
+import logo from './logo.png'
+
+/*function onClickStudent() {
+    window.location.href = "../login2/login2_stu.html";
 }
 
-function onClickStaff()
-{
-    window.location.href="../login2/login2_fac.html";
+function onClickStaff() {
+    window.location.href = "../login2/login2_fac.html";
 }
 
-function onClickAdmin()
-{
-    window.location.href="../login2/login2_Ad.html";
+function onClickAdmin() {
+    window.location.href = "../login2/login2_Ad.html";
+}*/
+
+const Login1 = () => {
+    return (
+        <div class="container">
+            <img src={logo} alt="IIT Tirupati Logo" />
+            <hr />
+            <h3>Welcome to IIT Tirupati E-Governance</h3>
+            <hr />
+            <div class="btn-group">
+                <button type="button" class="btn btn-outline-primary" onClick="onClickStaff()">Student Login</button>
+                <button type="button" class="btn btn-outline-primary" onClick="onClickStaff()">Faculty Login</button>
+                <button type="button" class="btn btn-outline-primary" onClick="onClickStaff()">Admin Login</button>
+                {/* < button class="button" onClick="onClickStudent()">Student Login</> -->
+                    <!-- <button class="button" onClick="onClickStaff()">Faculty Login</button>
+    <button class="button" onClick="onClickAdmin()">Admin Login</button> -->*/}
+            </div>
+        </div>
+    );
 }
+
+export default Login1;
