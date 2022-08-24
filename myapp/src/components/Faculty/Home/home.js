2ebf65ce775b7404d3c36b33fa962bc3c812094d
Pooja Sree <84029615+poojasree06@users.noreply.github.com>
2022-08-24 20:53:16 +0530

    template page added

--- myapp/src/components/Faculty/Home/home.js
+++ myapp/src/components/Faculty/Home/home.js
@@ -1,67 +1,13 @@
-/*function onClickProject()
-{
-    window.location.href="../CreateProject/create.html";
-}*/
+var li_items = document.querySelectorAll(".sidebar ul li");
+var menu_icon = document.querySelector(".menu_icon");
+var wrapper = document.querySelector(".wrapper");
 
-import React from "react";
-import { Link } from "react-router-dom"
-import "./home.css";
+//var year=new Date().getFullYear();
+//var year2=year+1;
+//document.getElementById("year").innerHTML=year+" - "+year2
 
-const Home = () => {
-    return (
-        <div >
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <Link className="navbar-brand" href="">Username</Link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div>
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <Link className="nav-link" href="">Workshops/Seminars/Lectures</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="">Awards/Recognitions</Link>
-                                <Link class="nav-link" href="">Journal/Conferences</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="">Books/Chapters</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="">Projects Guided</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="">Projects/Consultancy</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <div class="grid-container">
-                <div>
-                    <div class="container-box">
-                        <h4>Create Project</h4>
-                        <hr />
-                        <div class="">
-                            <button class="button">Sponsered/Consultancy Project</button>
-                            <button class="button">Internal Project</button>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="container-box">
-                        <h4>Recruitment</h4>
-                        <hr />
-                        <div class="">
-                            <button class="btn-staff">Staff</button>
-                            <button class="button">Adhoc</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
 
-export default Home;
+menu_icon.addEventListener("click", () => {
+	menu_icon.closest(".wrapper").classList.toggle("click_collapse");
+	menu_icon.closest(".wrapper").classList.toggle("hover_collapse");
+})
