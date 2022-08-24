0f16284630772af7d822ba787d1a726567e41c95
Preethi Varsha <82671452+PreethiVarshaM@users.noreply.github.com>
2022-08-24 23:41:15 +0530

    App update

--- myapp/src/components/App.jsx
+++ myapp/src/components/App.jsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-//import Home from "./Faculty/Home/home.js";
-//import LoginFac from "./login2/login2_fac";
+import Home from "./Faculty/Home/home.js";
+import LoginFac from "./login2/login2_fac";
 import Login1 from "./login1/login1.jsx";
 
 const App = () => {
@@ -9,6 +9,8 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login1 />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/loginfac" element={<LoginFac />} />
       </Routes>
     </BrowserRouter>
   );
