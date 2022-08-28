5d079ec7595b536e00d32ea089a097ede74ebe56
Preethi Varsha <82671452+PreethiVarshaM@users.noreply.github.com>
2022-08-28 18:03:41 +0530

    enabling interface between front and back end

--- myapp/src/components/Faculty/CreateProject/create.jsx
+++ myapp/src/components/Faculty/CreateProject/create.jsx
@@ -1,66 +1,111 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Sidebar from "../../Navbar/components/Sidebar";
 import Nav from "../../Navbar/components/Nav";
+import { Grid } from '@mui/material';
+
+
 export default function Create() {
-  return (
-    <div className="wrapper hover_collapse">
-  <Nav user="User Name"></Nav>
-  <Sidebar></Sidebar>
-    <form class="forms" style={{marginTop: '100px'}} >
-        <div class="">
-            <div class="first-row">
-                <div class="ag-code">
-                    <p>Agency ID</p>
-                    <select name="ag-code" id="ag-code"></select>
+    const [agencyCode, setagencyCode] = useState("0");
+    const [agencyName, setagencyName] = useState("default");
+    const [projectName, setProjectName] = useState();
+    const [projectID, setProjectID] = useState();
+    const [projectType, setprojectType] = useState("default")
+    const [organizationType, setorganizationType] = useState("default")
+    const [sanctionValue, setsanctionValue] = useState(0)
+    const [startDate, setStartDate] = useState("0/0/0")
+    const [endDate, setEndDate] = useState("0/0/0")
+    const [descriptionBox, setDescriptionBox] = useState("default text")
+
+    function onSubmit() { console.log("Submitted") }
+    //console.alert("SubmittedValues: ", projectID, " ", projectName, " ", projectType, organizationType, agencyCode, agencyName, sanctionValue, startDate, endDate, descriptionBox)
+
+    const postData = () => {
+        console.log("Posted!")
+        fetch("/created", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                projectName: projectName,
+                projectID: projectID,
+                projectType: projectType,
+                agencyCode: agencyCode,
+                agencyName: agencyName,
+                organizationType: organizationType,
+                sanctionValue: sanctionValue,
+                startDate: startDate,
+                endDate: endDate,
+                descriptionBox: descriptionBox
+            }),
+        }).then((res) => {
+            console.log("Bad Preethi Res:", res);
+        })
+    }
+
+    return (
+        <div className="wrapper hover_collapse">
+            <Nav user="User Name"></Nav>
+            <Sidebar></Sidebar>
+            <form className="forms" onSubmit={onSubmit} style={{ marginTop: '100px' }} >
+
+                <label>
+                    Project Name
+                    <input type="text" name="projectName" placeholder="Enter your project name" onChange={(event) => { setProjectName(event.target.value) }} />
+                </label>
+
+                <label>
+                    Project ID
+                    <input type="text" name="projectID" onChange={(event) => { setProjectID(event.target.value) }} />
+                </label>
+
+                <label>
+                    Project Type
+                    <input type="text" name="projectType" placeholder="Enter your project Type" onChange={(event) => { setprojectType(event.target.value) }} />
+                </label>
+
+
+                <label>
+                    Agency Code
+                    <input type="text" name="agencyCode" placeholder="Enter your Agency Code" onChange={(event) => { setagencyCode(event.target.value) }} />
+                </label>
+
+                <label>
+                    Agency Name
+                    <input type="text" name="agencyName" placeholder="Enter your Agency Name" onChange={(event) => { setagencyName(event.target.value) }} />
+                </label>
+
+                <label>
+                    Organization Type
+                    <input type="text" name="organizationType" placeholder="Enter your organization Type" onChange={(event) => { setorganizationType(event.target.value) }} />
+                </label>
+
+                <label>
+                    Sanction Value
+                    <input type="text" name="sanctionValue" placeholder="Enter your Sanction value" onChange={(event) => { setsanctionValue(event.target.value) }} />
+                </label>
+
+                <label>
+                    Start Date
+                    <input type="text" name="startDate" placeholder="Enter start date" onChange={(event) => { setStartDate(event.target.value) }} />
+                </label>
+
+                <label>
+                    End Date
+                    <input type="text" name="endDate" placeholder="Enter End date" onChange={(event) => { setEndDate(event.target.value) }} />
+                </label>
+
+                <label>
+                    Description
+                    <input type="text" name="Description" placeholder="Give a brief description of the project" onChange={(event) => { setDescriptionBox(event.target.value) }} />
+                </label>
+
+                <div className="btn">
+                    <button type="button" onClick={postData}>Create</button>
+                    {/* <button type="submit" onClick={getData}>GetData</button> */}
                 </div>
-                <div class="ag-name">
-                    <p>Agency name</p>
-                    <select name="ag-name" id="ag-name"></select>
-                </div>
-                <div class="og-type">
-                    <p>Organization Type</p>
-                    <select name="og-type" id="og-type"></select>
-                </div>
-                
-            </div>
-            <div class="second-row">
-                <div class="proj-id">
-                    <p>Project ID</p>
-                    <input type="text" placeholder="Project ID" name="proj-id"/>
-                </div>
-                <div class="proj-type">
-                    <p>Project Type</p>
-                    <input type="text" placeholder="Project Type" name="proj-type"/>
-                </div>
-                <div class="proj-name">
-                    <p>Project Name</p>
-                    <input type="text" placeholder="Project Name" name="proj-name"/>
-                </div>
-            </div>
-            <div class="third-row">
-                <div class="sanc-val">
-                    <p>Sanction Value</p>
-                    <input type="text" placeholder="Sanction Value" name="sanc-val"/>
-                </div>
-                <div class="start-d">
-                    <p>Start Date</p>
-                    <input type="date" id="start-d" name="start-d"/>
-                </div>
-                <div class="end-d">
-                    <p>End Date</p>
-                    <input type="date" id="end-d" name="end-d"/>
-                </div>
-            </div>
-            <div class="desc-box">
-                <p>Scope and Objective of the project</p>
-                <textarea id="desc-box" name="desc-box"  placeholder="Enter Scope and Objective here.."></textarea>
-            </div>
-            <div class="btn">
-                <button type="submit">Create</button>
-                <button type="submit">Cancel</button>
-            </div>
+
+            </form>
         </div>
-    </form>
-    </div>
-  )
+    )
 }
--- server.js
+++ server.js
@@ -1,47 +1,62 @@
-//jshint esversion:6
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import lodash from 'lodash';
+import cors from 'cors';
+
 const _ = { lodash }
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
 app.use(express.static("public"));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: ["POST", "GET", "PUT", "HEAD", "OPTIONS"],
+    })
+);
 
-mongoose.connect("mongodb://localhost:27017/SRC", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/SRC", { useNewUrlParser: true }, () => { console.log("connected to database") });
 
 const projectsSchema = new mongoose.Schema({
     projectCode: String,
     projectName: String,
+    agencyCode: String,
+    agencyName: String,
     approval: Boolean,
     resourceApproval: Boolean,
     fundApproval: Boolean,
     closed: Boolean,
     facultyID: String,
+    organizationType: String,
     staff: [mongoose.Types.ObjectId],
     sanctionFund: Number,
     startDate: Date,
     endDate: Date,
     status: Number,
+    description: String
 
 })
 
-const Project = mongoose.model("Item", projectsSchema)
+const Project = mongoose.model("Project", projectsSchema)
 
 //dummy project
 const project1 = new Project({
     projectCode: "Fa01",
     projectName: "FaName",
+    agencyName: "Agency",
+    agencyCode: "Number",
     approval: true,
     resourceApproval: true,
     fundApproval: true,
     closed: false,
     facultyID: "FaID",
-    startDate: 18 / 12 / 2022,
-    endDate: 20 / 12 / 2022,
+    startDate: 18 - 12 - 2022,
+    endDate: 20 - 12 - 2022,
     //staff: [],
     sanctionFund: 100000,
     status: 20,
@@ -58,6 +73,7 @@ const facultySchema = new mongoose.Schema({
     projects: [mongoose.Types.ObjectId]
 
 });
+const Faculty = mongoose.model('Faculty', facultySchema);
 
 const adminSchema = new mongoose.Schema({
     username: {
@@ -68,9 +84,52 @@ const adminSchema = new mongoose.Schema({
     password: String,
     projects: {
         approve: [projectsSchema],
-    }
+    },
+    faculty: []
 })
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+const Admin = mongoose.model('Admin', adminSchema);
+
+
+
+
+
+app.get("/created", (req, res, err) => {
+    res.send('Bad Preethi Server Online!!!!')
+})
+
+app.post("/created", (req, res) => {
+    console.log("Recieved?");
+    console.log("Body:", req.body);
+    res.send("Please Please Die @PreethiVarsha")
+    //console.log(result)
+    // const newProject = await Project.create({
+    //     projectCode: req.body.projectCode,
+    //     projectName: req.body.projectName,
+    //     agencyCode: req.body.agencyCode,
+    //     agencyName: req.body.agencyCode,
+    //     organizationType: req.body.organizationType,
+    //     approval: false,
+    //     resourceApproval: false,
+    //     fundApproval: false,
+    //     closed: false,
+    //     facultyID: req.body.facultyID,
+    //     staff: [],
+    //     sanctionFund: req.body.sanctionFund,
+    //     startDate: req.body.startDate,
+    //     endDate: req.body.endDate,
+    //     status: 0,
+    //     description: req.body.description
+    // })
+
+    // res.status(200).json(newProject)
+
+});
+
+let port = 3001;
+// if (port == null || port == "") {
+//     port = 3001;
+// }
+app.listen(port, function () {
+    console.log("Server started on port 3001");
 });
