0baf36911f9c17b361371e53c88dcbd62fe484f4
Namitha Reddy <namithareddy066@gmail.com>
2022-08-30 20:56:03 +0530

    commit

--- myapp/src/components/Admin/Approve_projects.jsx
+++ myapp/src/components/Admin/Approve_projects.jsx
@@ -94,12 +94,23 @@ const Approve_project = () => {
      description: "This is description regarding project"},
   ]
 
+//   {domains.map((user) => {
+//     let keys = Object.keys(user);
+//     return (
+//       <Text style={styles.paragraph}>{keys.map((key) => {
+//         return (
+//           <Text>{`Key ${key}, value ${user[key]} \n`}</Text>
+//         )
+//       })}</Text>
+//     )
+//   })}
+
   return (
     <div className="wrapper hover_collapse">
    <div className="grid" style={{marginLeft: "300px",marginTop: "50px"}}>
      <Nav user="User Name"></Nav>
      <Sidebar></Sidebar>
-    {cardinfo.map((card,index) => (
+     {cardinfo.map((card,index) => (
       <Card id={index} projectCode={card.projectCode} projectName ={card.agencyName} projectType={card.projectType} agencyCode={card.agencyCode} agencyName={card.agencyName} approval={card.approval} resourceApproval={card.resourceApproval} fundApproval={card.fundApproval} closed={card.closed} facultyID={card.facultyID} organisationType={card.organizationType} staff={card.staff} sanctionFund={card.sanctionFund} startDate={card.startDate} endDate={card.endDate} status={card.status} description={card.description} content="Approve Project"/>
     ))}
    </div>
--- myapp/src/components/App.jsx
+++ myapp/src/components/App.jsx
@@ -9,6 +9,13 @@ import Create from "../components/Faculty/CreateProject/create"
 import Announcements from "./Faculty/Announcements/Announcements.jsx";
 import Admin from "./Admin/Admin.jsx";
 import Pending from "./Faculty/Pending/Pending.jsx"
+import ApproveProject from "./Admin/Approve_projects.jsx";
+import ApproveRecruitment from "./Admin/Approve_Recruitmnet.jsx";
+import DurationExtension from "./Admin/Duration_Extension.jsx";
+
+
+
+
 const App = () => {
   return (
     <Router>
@@ -17,7 +24,9 @@ const App = () => {
         <>
         <Route exact path="/login2_Ad" element={<LoginAdmin />} />
         <Route exact path="/admin_home" element={<Admin/>} />
-
+        <Route exact path="/Approve_project" element={<ApproveProject />} />
+        <Route exact path="/approve_recruitment" element={<ApproveRecruitment />} />
+        <Route exact path="/duration_extension" element={<DurationExtension />} />
         </>
         <>
         <Route exact path="/login2" element={<LoginFac />} />
@@ -27,7 +36,7 @@ const App = () => {
           <Route exact path="/recruitment" element={<Recruitment />} />
           <Route exact path="/announcements" element={<Announcements />} />
           <Route exact path="/pending_approvals" element={<Pending />} />
-          </>
+        </>
       </Routes>
     </Router>
   );
