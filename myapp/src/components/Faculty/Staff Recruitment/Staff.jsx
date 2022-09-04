61aa62bd9648cf557f3c7746d16557f39a15ac28
CS20B019-Manasa <cs20b019@iittp.ac.in>
2022-09-04 10:41:17 +0530

    added staff recruitment form

---
+++ myapp/src/components/Faculty/Staff Recruitment/Staff.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useRef } from 'react'
+import Sidebar from "../../Navbar/components/Sidebar";
+import Nav from "../../Navbar/components/Nav";
+import "./staff.css"
+//import MainNav from '../../Navbar/components/MainNav';
+import { MainNav } from '../../App';
+//import { Container } from '@mui/system';
+
+class Staff extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            projectName: null,
+            projectID: null,
+            proejctType: null,
+            no_ofStaff: null,
+            salaryDetails: null,
+            startDate: null,
+            endDate: null,
+            descriptionBox: null
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(event) {
+        const target = event.target;
+        var value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+
+    }
+
+    submit() {
+        console.log("submiitted")
+        console.warn(this.state)
+    }
+    render() {
+        return (
+            <div className="wrapper hover_collapse">
+                <MainNav></MainNav>
+                <div className="main_container">
+                    <div className="container">
+                        <form>
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="projectName">project Name</label>
+                                    <input type="text" class="form-control" name="projectName" id="projectName" placeholder="Project Name" onChange={this.handleInputChange} />
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="projectID">Project ID</label>
+                                    <input type="text" class="form-control" name="projectID" id="projectID" placeholder="Project ID" onChange={this.handleInputChange} />
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="projectType">Project Type</label>
+                                    <input type="text" class="form-control" name="projectType" id="projectType" placeholder="Project Type" onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="no_ofStaff">Number of Staff</label>
+                                    <input type="number" class="form-control" name="no_ofStaff" id="no_ofStaff" placeholder="Number of Staff" onChange={this.handleInputChange} />
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="salaryDetails">Salary Details</label>
+                                    <input type="text" class="form-control" name="salaryDetails" id="salaryDetails" placeholder="Salary Details" onChange={this.handleInputChange} />
+                                </div>
+                                <div class="form-group col-md-2">
+                                    <label for="startDate">Start Date</label>
+                                    <input type="date" class="form-control" name="startDate" id="startDate" placeholder="Start Date" onChange={this.handleInputChange} />
+                                </div>
+                                <div class="form-group col-md-2">
+                                    <label for="endDate">End date</label>
+                                    <input type="date" class="form-control" name="endDate" id="endDate" placeholder="End Date" onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="descriptionBox">Reason for Recruitment</label>
+                                    <textarea type="text" class="form-control" rows="5" id="descriptionBox" placeholder="Description" onChange={this.handleInputChange} />
+                                </div>
+                                <div class="form-group col-md-4" style={{ marginTop: '50px', }}>
+                                    <button type="button" onClick={() => this.submit()} class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Staff;
