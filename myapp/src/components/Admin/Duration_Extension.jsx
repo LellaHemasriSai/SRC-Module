c27ff3b2109b8384e74f317cf2a1c27cdfa536af
Pooja Sree <84029615+poojasree06@users.noreply.github.com>
2022-09-04 20:57:15 +0530

    commit

--- myapp/src/components/Admin/Duration_Extension.jsx
+++ myapp/src/components/Admin/Duration_Extension.jsx
@@ -2,103 +2,38 @@ import Card from "./Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Faculty/ongoing/ongoing.css";
 import { AdminMainNav } from '../App';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 const Duration_Extension = () => {
-  const cardinfo = [
-    
-     { id:"1", projectCode: "2345",
-     projectName: "SRC",
-     projectType: "Web",
-     agencyCode: "X2450",
-     agencyName: "TCS",
-     approval: "0",
-     resourceApproval: "1",
-     fundApproval: "0",
-     closed: "0",
-     facultyID: "94350",
-     organizationType: "Global",
-     staff: ["nam","manas","hems"],
-     sanctionFund: "20000",
-     startDate: "01-01-2022",
-     endDate: "10-01-2022",
-     status: "1",
-     description: "This is description regarding project"},
-     { id:"2", projectCode: "2345",
-     projectName: "SRC",
-     projectType: "Web",
-     agencyCode: "X2450",
-     agencyName: "TCS",
-     approval: "0",
-     resourceApproval: "1",
-     fundApproval: "0",
-     closed: "0",
-     facultyID: "94350",
-     organizationType: "Global",
-     staff: ["nam","manas","hems"],
-     sanctionFund: "20000",
-     startDate: "01-01-2022",
-     endDate: "10-01-2022",
-     status: "1",
-     description: "This is description regarding project"},
-     { id:"3", projectCode: "2345",
-     projectName: "SRC",
-     projectType: "Web",
-     agencyCode: "X2450",
-     agencyName: "TCS",
-     approval: "0",
-     resourceApproval: "1",
-     fundApproval: "0",
-     closed: "0",
-     facultyID: "94350",
-     organizationType: "Global",
-     staff: ["nam","manas","hems"],
-     sanctionFund: "20000",
-     startDate: "01-01-2022",
-     endDate: "10-01-2022",
-     status: "1",
-     description: "This is description regarding project"},
-     { id:"4", projectCode: "2345",
-     projectName: "SRC",
-     projectType: "Web",
-     agencyCode: "X2450",
-     agencyName: "TCS",
-     approval: "0",
-     resourceApproval: "1",
-     fundApproval: "0",
-     closed: "0",
-     facultyID: "94350",
-     organizationType: "Global",
-     staff: ["nam","manas","hems"],
-     sanctionFund: "20000",
-     startDate: "01-01-2022",
-     endDate: "10-01-2022",
-     status: "1",
-     description: "This is description regarding project"},
-     { id:"5", projectCode: "2345",
-     projectName: "SRC",
-     projectType: "Web",
-     agencyCode: "X2450",
-     agencyName: "TCS",
-     approval: "0",
-     resourceApproval: "1",
-     fundApproval: "0",
-     closed: "0",
-     facultyID: "94350",
-     organizationType: "Global",
-     staff: ["nam","manas","hems"],
-     sanctionFund: "20000",
-     startDate: "01-01-2022",
-     endDate: "10-01-2022",
-     status: "1",
-     description: "This is description regarding project"},
-  ]
+
+  const [cardinfo, setCardInfo] = useState([])
+
+  console.log("Submitted")
+
+  useEffect(() => {
+    axios.post('http://localhost:3001/pending')
+      .then(res => {
+        console.log('Data: ', res.data.data)
+        setCardInfo(res.data.data)
+
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
 
   return (
     <div className="wrapper hover_collapse">
-   <div className="grid" style={{marginLeft: "300px",marginTop: "50px"}}>
     <AdminMainNav></AdminMainNav>
+    <div className="main_container">
+    <div className="container" style={{textAlign:"center"}}>
+    <div className='grid'>
     {cardinfo.map((card,index) => (
       <Card id={index} projectCode={card.projectCode} projectName ={card.agencyName} projectType={card.projectType} agencyCode={card.agencyCode} agencyName={card.agencyName} approval={card.approval} resourceApproval={card.resourceApproval} fundApproval={card.fundApproval} closed={card.closed} facultyID={card.facultyID} organisationType={card.organizationType} staff={card.staff} sanctionFund={card.sanctionFund} startDate={card.startDate} endDate={card.endDate} status={card.status} description={card.description} content="Extend Duration"/>
     ))}
+   </div>
+   </div>
    </div>
    </div>
   );
