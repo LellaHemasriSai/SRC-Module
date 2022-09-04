eabd72a10c58d1902c1ffcefa25297883f1649e6
Namitha Reddy <namithareddy066@gmail.com>
2022-09-04 17:44:20 +0530

    card update

--- myapp/src/components/Faculty/ongoing/Card.js
+++ myapp/src/components/Faculty/ongoing/Card.js
@@ -3,6 +3,8 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Card.css";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import "./statusbarstyles.css"
 
 const Cards = (props) => {
 
@@ -21,25 +23,26 @@ return (
               </Card.Text>
              { show?<div>
                 <ul class="list-group list-group-flush">
-                <li class="list-group-item">projectType:{props.projectType}</li>
-                <li class="list-group-item">agencyCode:{props.agencyCode}</li>
-                <li class="list-group-item">agencyName:{props.agencyName}</li>
-                <li class="list-group-item">approval:{props.approval}</li>
-                <li class="list-group-item">resourceApproval:{props.resourceApproval}</li>
-                <li class="list-group-item">fundApproval:{props.fundApproval}</li>
-                <li class="list-group-item">closed:{props.closed}</li>
-                <li class="list-group-item">facultyID:{props.facultyID}</li>
-                <li class="list-group-item">organisationType:{props.organisationType}</li>
-                <li class="list-group-item">staff:{props.staff}</li>
-                <li class="list-group-item">sanctionFund:{props.sanctionFund}</li>
-                <li class="list-group-item">startDate:{props.startDate}</li>
-                <li class="list-group-item">endDate:{props.endDate}</li>
-                <li class="list-group-item">status:{props.status}</li>
-                <li class="list-group-item">_v:{props._v}</li>
-                <li class="list-group-item">announcements:{props.announcements}</li>
+                <li class="list-group-item">projectType -&ensp;{props.projectType}</li>
+                <li class="list-group-item">agencyCode -&ensp;{props.agencyCode}</li>
+                <li class="list-group-item">agencyName -&ensp;{props.agencyName}</li>
+                <li class="list-group-item">approval -&ensp;{props.approval ? "True" : "False" }</li>
+                <li class="list-group-item">resourceApproval -&ensp;{props.resourceApproval ? "True" : "False"}</li>
+                <li class="list-group-item">fundApproval -&ensp;{props.fundApproval ? "True" : "False"}</li>
+                <li class="list-group-item">closed -&ensp;{props.closed ? "True" : "False"}</li>
+                <li class="list-group-item">facultyID -&ensp;{props.facultyID}</li>
+                <li class="list-group-item">organisationType -&ensp;{props.organisationType}</li>
+                <li class="list-group-item">staff -&ensp;{props.staff}</li>
+                <li class="list-group-item">sanctionFund -&ensp;{props.sanctionFund}</li>
+                <li class="list-group-item">startDate -&ensp;{props.startDate}</li>
+                <li class="list-group-item">endDate -&ensp;{props.endDate}</li>
+                <li class="list-group-item">status -&ensp;{props.status.toString()}</li>
+                {/* <li class="list-group-item">_v -&ensp;{props._v}</li> */}
+                <li class="list-group-item">announcements -&ensp;{props.announcements}</li>
               </ul></div>
                   : null
              }
+             <CircularProgressbar value={props.status} text={`${props.status}%`} className="status"/>
               <Button variant="primary" className="butt" onClick={() =>setShow(!show)}>{show?"Read Less":"Read More"}</Button>
             </Card.Body>
           </Card>
