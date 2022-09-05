fa451d393957d359f12a20d1bc83092206b1f8cb
Namitha Reddy <namithareddy066@gmail.com>
2022-09-05 15:19:44 +0530

    changed css in application

--- myapp/src/components/Faculty/Applications.jsx
+++ myapp/src/components/Faculty/Applications.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useRef } from 'react'
 //import Sidebar from "../../Navbar/components/Sidebar";
 //import Nav from "../../Navbar/components/Nav";
-//import "./staff.css"
 //import MainNav from '../../Navbar/components/MainNav';
+import "../Faculty/Staff Recruitment/staff.css"
 import { MainNav } from '../App'
 
 
@@ -15,14 +15,16 @@ export default function ExtendDuration() {
                 <div class="content">
           <h2>Application - 1</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <button>Accept</button>
-          <button>Reject</button>
+          <div className='buttgroup'>
+          <button className='button2'>Accept</button>
+          <button className='button2'>Reject</button>
+          </div>
 			</div>
             <div class="content">
           <h2>Application - 2</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <button>Accept</button>
-          <button>Reject</button>
+            <button className='button2'>Accept</button>
+          <button className='button2'>Reject</button>
 			</div>
                 </div>
             </div>
