3bfbfacb3b452cd8ed3621ca7c809a232e612a0d
Pooja Sree <84029615+poojasree06@users.noreply.github.com>
2022-09-09 18:49:40 +0530

    changes in student page

--- myapp/src/components/Staff/StudentCard.jsx
+++ myapp/src/components/Staff/StudentCard.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import "./staff.css"
 
+function onApply(e) {
+  console.log("Applied") }
+
 export default function StudentCard(props) {
+  const [ state, setState ] = useState([]);
+
+    function addApplication(e) {
+    const object = { name: '',id:'', faculty:'',quals:''};
+    setState([...state, object]);
+  }
+
   return (
-    <li class="cards_item">
+ <li class="cards_item">
       <div class="card">
-        
-          <h2 class="card_title">Card Grid Layout</h2>
-		<div className='card_text'>
-		<p>Project Name: Title</p>
-		<p>Project ID: Title</p>
-		<p>Faculty Name: Title</p>
-		<p>Name: Title</p>
-		</div>
-          <button class="btn card_btn">Apply Now</button>
-        
+        <div class="card_content">
+          <br></br>
+          <p class="card_text">
+          <span className="head_content">Project Code</span><span>{props.id}</span><br></br><br></br>
+          <span className="head_content">Project Name</span><span>{props.name}</span><br></br><br></br>
+          <span className="head_content">Faculty Name</span><span>{props.faculty}</span><br></br><br></br>
+          <span className="head_content">Qualifications</span><span>{props.quals}</span><br></br><br></br>
+          <span className="head_content" >Salary Details</span><span></span><br></br>
+          </p>
+          <button class="btn card_btn" onClick={onApply
+          ()}>Apply Now</button>
+        </div>
       </div>
     </li>
   );
