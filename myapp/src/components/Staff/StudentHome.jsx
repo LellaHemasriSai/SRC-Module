5b6d98c948d21142c52f7ea572828fd0cc28dcec
Pooja Sree <84029615+poojasree06@users.noreply.github.com>
2022-09-09 21:24:40 +0530

    layput change in student page

--- myapp/src/components/Staff/StudentHome.jsx
+++ myapp/src/components/Staff/StudentHome.jsx
@@ -1,32 +1,31 @@
 import React from 'react'
 import Container from '../Navbar/components/Container';
 import {StudentMainNav} from '../App.jsx'
+import HomeCard from "./HomeCard.jsx"
+const homedata=[
+  {
+    key:1,                 /* data must have an unique key */
+    projectName:'xyz',
+    projectCode:'',
+    projectType:'',
+    FacultyName:'',
+    startDate:'',
+    endDate:'',
+    salary:'',
+    description:'',
+  }
+]
 function StudentHome(props) {
   return <div className="wrapper hover_collapse">
   <StudentMainNav></StudentMainNav>
   	<div class="main_container">
 		<div class="container">
-    <div className='student_content'>
-      <div class="content">
-          <h2>Announcement - 1</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <button>Apply Now</button>
-			</div>
-      </div>
-      <div className='student_content'>
-      <div class="content">
-				  <h2>Announcement - 2</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <button>Apply Now</button>
-			</div>
-      </div>
-      <div className='student_content'>
-      <div class="content">
-				          <h2>Announcement - 3</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <button>Apply Now</button>
-			</div>
-       </div>
+    <HomeCard></HomeCard>
+    {
+      homedata.map=(h)=>{
+        <HomeCard></HomeCard>
+      }
+    }
 		</div>
 	</div>
   </div>
