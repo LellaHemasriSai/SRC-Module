edd0a69f0f8de9fbf0f15df88b5b26373a2ea61b
Pooja Sree <84029615+poojasree06@users.noreply.github.com>
2022-09-12 23:54:09 +0530

    deleted unnecessary files

--- myapp/src/components/Navbar/components/Container.jsx
+++ myapp/src/components/Navbar/components/Container.jsx
@@ -1,44 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-//import NavBar from './Navbar';
-import Content from './content';
-// const data=[{
-//         "Name": "Pooja",
-//         "Department": "CSE",
-//         "Email": "CS20B002@iittp.ac.in",
-//         "Contact":"99999999999",
-//         "DateOfJoining":"10/12/2020",
-//         "Qualifications":"b-tech",
-//         "DoB":"09/06/2003"
-//     },
-//     {
-//         "Name": "ASM",
-//         "Department": "CSE",
-//         "Email": "CS20B00x@iittp.ac.in",
-//         "Contact":"99999999999",
-//         "DateOfJoining":"10/12/2020",
-//         "Qualifications":"b-tech",
-//         "DoB":"09/06/2003"
-//     }
-// ]
 
-const user={
-      "Name": "Pooja Sree",
-      "DoB":"09/06/2003",
-      "Department": "CSE",
-      "Email": "CS20B002@iittp.ac.in",
-      "Contact":"8247800018",
-      "DoF":"10/12/2020",
-      "Qualifications":"B-tech",
-      "Address":"Vijayawada"
-      
-}
-export default function Container(props) {
-  const {username}=useParams();
+export default function Container() {
   return (
     <div className="main_container">
 		<div className="container">
-		<Content/> 
 		</div>
 	</div>
   );
