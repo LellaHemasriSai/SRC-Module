e6c52450df35fb6dc6623f5cc0a73f14ed728b3a
Pooja Sree <84029615+poojasree06@users.noreply.github.com>
2022-09-13 22:18:43 +0530

    student ongoing frontend

--- myapp/src/components/Staff/StudentOngoing.jsx
+++ myapp/src/components/Staff/StudentOngoing.jsx
@@ -1,42 +1,24 @@
 import React from 'react'
 import {StudentMainNav} from '../App.jsx'
 import "./ongoing.css"
+import ProjectCard from './ProjectCard.jsx';
+import ongoing from "../../images/ongoing.svg"
+
 function StudentOngoing(props) {
   return <div className="wrapper hover_collapse">
   <StudentMainNav></StudentMainNav>
   	<div className="main_container">
-		<div className="container">
-   
-<section class="card-container">
-	<div class="card_ongoing">
-		<h1>Card #1</h1>
-	</div>
-
-	<div class="card_ongoing">
-		<h1>Card #2</h1>
-	</div>
-
-	<div class="card_ongoing">
-		<h1>Card #3</h1>
-	</div>
-	
-	<div class="card_ongoing">
-		<h1>Card #4</h1>
-	</div>
-
-	<div class="card_ongoing">
-		<h1>Card #5</h1>
-	</div>
-
-	<div class="card_ongoing">
-		<h1>Card #6</h1>
-	</div>
-
-</section>
-
+	<div  className='head_wrap' style={{marginLeft:"10%"}}>
+            <span><img src={ongoing} style={{ width: "35px", height: "35px" }} alt="completed"/></span>
+            <span><h1 id="head_text">Ongoing Projects</h1></span>
+    </div>
+	<section class="card-container">
+		<ProjectCard></ProjectCard>
+		<ProjectCard></ProjectCard>
+		<ProjectCard></ProjectCard>
+	</section>
 	</div>
 	</div>
-  </div>
 }
 
 export default StudentOngoing;
