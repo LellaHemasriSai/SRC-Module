989aa8f8a14141d4226ddfde9e44d5e55a23b8de
Namitha Reddy <namithareddy066@gmail.com>
2022-09-13 18:16:09 +0530

    Registration form added navbar

--- myapp/src/components/Faculty/Form.jsx
+++ myapp/src/components/Faculty/Form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 //import { Grid } from '@mui/material';
 //import Button from '@mui/material';
-//import { MainNav } from '../../App';
+import { MainNav } from '../App';
 
 export default function RegisterForm() {
     const [Department, setDepartment] = useState("");
@@ -57,22 +57,17 @@ export default function RegisterForm() {
 
 
     return (
-        <div>
+        <div className="wrapper hover_collapse" >
+        <MainNav></MainNav>
+        <div className="main_container">
+          <div className="container" >
+            {/* <div className='grid'> */}
             <div class="row">
                 <div class="col-md-6 offset-md-3">
                     <br /><br />
-                    <h1>Register Form</h1><br />
+                    <h1>Register Form</h1>
+                    <br />
                     <form>
-                        {/*<div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label>First Name</label>
-                                    <input type="text" class="form-control" name="first_name" onChange={this.handleInputChange} />
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label>Last Name</label>
-                                    <input type="text" class="form-control" name="last_name" onChange={this.handleInputChange} />
-                                </div>
-                            </div>*/}
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label>Department</label>
@@ -107,19 +102,6 @@ export default function RegisterForm() {
                                 <input type="date" class="form-control" id="DoB" value={DoB} placeholder="DoB" onChange={(event) => { setDoB(event.target.value) }} />
                             </div>
                         </div>
-                        {/*<div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label>Gender</label><br />
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="Gender" id="inlineRadiom" value="male" checked={this.state.gender === "male"} onChange={this.handleInputChange} />
-                                        <label class="form-check-label" for="inlineRadiom">Male</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="gender" id="inlineRadiof" value="female" checked={this.state.gender === "female"} onChange={this.handleInputChange} />
-                                        <label class="form-check-label" for="inlineRadiof">Female</label>
-                                    </div>
-                                </div>
-                        </div>*/}
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label>Address </label>
@@ -143,5 +125,8 @@ export default function RegisterForm() {
                 </div>
             </div>
         </div>
+        </div>
+        </div>
+        // </div>
     )
 }
