77c0e80ea982b3983bf54049c45ac9f2ae8f9a0a
Namitha Reddy <namithareddy066@gmail.com>
2022-09-15 00:35:08 +0530

    Create a basic tax calc card in completed projects

--- myapp/src/components/Faculty/Completed/Tax_Calculator.js
+++ myapp/src/components/Faculty/Completed/Tax_Calculator.js
@@ -1,83 +1,54 @@
-import React from "react";
+import React,{useState} from "react";
 import "./tax.css";
+import { MainNav } from '../../App';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-export default function Tax_Calculator(props){ 
-
-    var taxval = props.cost;
+export default function Tax_Calculator(props)
+{ 
+    var cost = 10000;
+    var taxval = cost;
     taxval=taxval*(12/100);
-    var finalval = props.cost + taxval;
-
-<div id="calculatorWrap">
-    <form action="service-tax-calculator-india.php" method="post" class="calculatorForm" id="CalculatorForm" name="CalculatorForm" onsubmit="calculateButtonWorking()">
-        <div itemprop="name" class="calculatorTitle">India Service Tax</div>
-        <div class="calculatorInputs">
-            <div class="table commonTable">
-                <div class="row">
-                    <div class="cell">
-                        <label for="amount">Amount Rs.:</label>
-                    </div>
-                    <div class="cell">
-                        <input name="amount" id="amount" value={props.cost} class="nmbr_real real" type="text" title="positive real number" placeholder="amount" required autofocus />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="cell">
-                        <input type="checkbox" name="inclusive" id="inclusive" />
-                    </div>
-                    <div class="cell">
-                        <label for="inclusive"><span class="note">Amount includes taxes</span></label>
-                    </div>
-                </div>
-            </div>
-            <fieldset>
-                <legend>Tax Rates</legend>
-                <div class="table commonTable">
-                    <div class="row">
-                        <div class="cell">
-                            <label for="service">Service Tax:</label>
-                        </div>
-                        <div class="cell">
-                            <input name="service" id="service" value="12" class="nmbr_real real" type="text" title="0 or positive real number" placeholder="Service Tax" /> %
-                        </div>
-                    </div>
-            
-                </div>
-            </fieldset>
-        </div>
-                                    <div class="calculatorButton floatRight" id="actionCalculate">
-            <input type="submit" value="Calculate" class="btn" name="calculateButton" id="calculateButton"/>
-        </div>
-        <div class="calculatorButton floatLeft" id="actionClear">
-            <input type="reset" name="reset" value="Clear" class="btn" onclick="return resetCalculator(this.form);"/>
-        </div>
-        <div class="clearBoth"></div>
-        <div id="calculatorAnswer">Answer:
-            <br/>
-            <div id="answer">
-<div class="table prettytable centered">
-<div class="row">
-<div class="cell alignRight">Gross Value: </div>
-<div class="cell alignRight">Rs. {props.cost}</div>
-</div>
-<div class="row">
-<div class="cell alignLeft"><strong>Taxes</strong></div>
-<div class="cell alignRight">&nbsp;</div>
-</div>
-<div class="row">
-<div class="cell alignRight">Service Tax (12%): </div>
-<div class="cell alignRight">{taxval}</div>
-</div>
-<div class="row">
-<div class="cell alignRight"><strong>Gross Charge:</strong> </div>
-<div class="cell alignRight"><strong>{finalval}</strong></div>
-</div>
-
-</div>
-</div>
-        </div>
-        <input type="hidden" name="action" value="solve" />
-        <input type="hidden" name="page_id" value="MTY2MzA4NDgwMg==" />
-    </form>
+    var finalval = cost + taxval;
+    const [click,setClick] = useState(false);
+   
+return (
+<>
+<div div className="wrapper hover_collapse" >
+      <MainNav></MainNav>
+      <div className="main_container">
+<div class="container">
+<Card style={{width: "40rem",height: "40rem",marginLeft: "35%"}}>
+  <form id="taxCalc" action="#">
+    <h1 class="h1">Tax Calculator</h1>
+    <div class="tax-input-container">
+      <div class="tax-input tax-input--income">
+        <label class="lab">Amount</label>
+        <input id="income" type="number" placeholder="125000" value={cost}/>
+      </div>
+      <div class="tax-input tax-input--ser">
+        <label class="lab">Tax </label>
+        <input type="text" readonly="true" id="service-tax" step="0.01" placeholder="12%"/>
+      </div>
+      <div class="tax-input tax-input--ser">
+        <label class="lab">Tax Amount</label>
+        <input type="number" id="service-tax-amount"  placeholder="100" value={click? taxval: "0"}/>
+      </div>
+      <div class="tax-input tax-input--ser">
+        <label class="lab">Gross Amount</label>
+        <input type="number" id="service-tax-amount"  placeholder="1000" value={click? finalval: "0"}/>
+      </div>
     </div>
+
+    <div class="tax-input-container">
+      <Button type="submit" className="forbutt" variant="primary" onClick={() => setClick(true)}>Calculate</Button>
+    </div>
+  </form>
+  </Card>
+</div>
+</div>
+</div>
+</>
+)
 
 }
