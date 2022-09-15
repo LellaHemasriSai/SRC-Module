382c1def2294452b5fc0c8c02759b545d5840474
Preethi Varsha <82671452+PreethiVarshaM@users.noreply.github.com>
2022-09-15 19:19:36 +0530

    close projects based on status = 100 updated

--- myapp/src/components/Staff/StudentCard.jsx
+++ myapp/src/components/Staff/StudentCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./staff.css"
 
@@ -6,11 +6,11 @@ import "./staff.css"
 
 
 export default function StudentCard(props) {
-    const [username, setUserName] = useState([])
-    const [email,setEmail]=useState([])
-    const [qual,setQual]=useState([])
-    const [dep,setDep]=useState([])
-    const [contact,setContact]=useState([])
+  const [username, setUserName] = useState([])
+  const [email, setEmail] = useState([])
+  const [qual, setQual] = useState([])
+  const [dep, setDep] = useState([])
+  const [contact, setContact] = useState([])
   useEffect(() => {
     axios.post('http://localhost:3001/sendStaffDetails')
       .then(res => {
@@ -27,72 +27,72 @@ export default function StudentCard(props) {
   }, [])
 
 
-    function postApplication(id) {
-        console.log("Applied")
-        props.onHandle(id);
-        fetch("/sendApplications", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                staffName:username,
-                projectID:id,
-                department:dep,
-                email:email,
-                qualifications:qual,
-                contact:contact,
-            }),
-        }).then((res) => {
-            console.log("Res:", res);
-        })
-    }
+  function postApplication(id) {
+    console.log("Applied")
+    props.onHandle(id);
+    fetch("/saveApplications", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        staffName: username,
+        projectID: id,
+        department: dep,
+        email: email,
+        qualifications: qual,
+        contact: contact,
+      }),
+    }).then((res) => {
+      console.log("Res:", res);
+    })
+  }
 
-  let start=new Date(props.start);
-  let sDate=start.getDay()+"/"+start.getMonth()+"/"+start.getFullYear();
-  let end=new Date(props.end);
-  let eDate=end.getDay()+"/"+end.getMonth()+"/"+end.getFullYear();
+  let start = new Date(props.start);
+  let sDate = start.getDay() + "/" + start.getMonth() + "/" + start.getFullYear();
+  let end = new Date(props.end);
+  let eDate = end.getDay() + "/" + end.getMonth() + "/" + end.getFullYear();
 
-  
+
   return (
     <div className='student_content'>
       <div class="content">
-      <div className='header_text'>
-        <span style={{ fontSize:"2.3rem" , fontWeight:"900"}}>{props.name}</span>
-      </div>
-          <div>
-            <span>
-              {props.id}
-            </span>
-          </div>
-            <div>
-            <span>
-              {props.type}
-            </span>
-          </div>
-          <div className='application_text'>
-            <span className='application_items'>
-              <p className='items_p'>Faculty Name</p>
-              <span>faculty</span>  {/**faculty name from backend??? */}
-              </span>
-            <span className='application_items'>
-              <p className='items_p'>Salary Details</p>
-              <p>{props.details}</p>
-            </span>
-            <span className='application_items'>
-              <p className='items_p'>Start Date</p>
-              <p>{sDate}</p>
-            </span>
-            <span className='application_items'>
-              <p className='items_p'>End Date</p>
-              <p>{eDate}</p>
-            </span>
-          </div>
-      <div className='home_text'>
+        <div className='header_text'>
+          <span style={{ fontSize: "2.3rem", fontWeight: "900" }}>{props.name}</span>
+        </div>
+        <div>
+          <span>
+            {props.id}
+          </span>
+        </div>
+        <div>
+          <span>
+            {props.type}
+          </span>
+        </div>
+        <div className='application_text'>
+          <span className='application_items'>
+            <p className='items_p'>Faculty Name</p>
+            <span>faculty</span>  {/**faculty name from backend??? */}
+          </span>
+          <span className='application_items'>
+            <p className='items_p'>Salary Details</p>
+            <p>{props.details}</p>
+          </span>
+          <span className='application_items'>
+            <p className='items_p'>Start Date</p>
+            <p>{sDate}</p>
+          </span>
+          <span className='application_items'>
+            <p className='items_p'>End Date</p>
+            <p>{eDate}</p>
+          </span>
+        </div>
+        <div className='home_text'>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <button onClick={() => { postApplication(props.id) }} className='apply_btn'>Apply Now</button>
+      </div>
     </div>
-    <button onClick={() => { postApplication(props.id) }} className='apply_btn'>Apply Now</button>
-	</div>
-  </div>
   )
 };
