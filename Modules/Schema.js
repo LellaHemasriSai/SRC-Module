5a044d9d041d0d9fb900a1aba52948722ffdc391
Preethi Varsha <82671452+PreethiVarshaM@users.noreply.github.com>
2022-10-02 10:32:16 +0530

    Separated mongodb schemas into a different file

---
+++ Modules/Schema.js
@@ -0,0 +1,239 @@
+import mongoose from "mongoose";
+
+//-----------------------------------------------------------------------------
+//SCHEMAS
+//-----------------------------------------------------------------------------
+
+const projectsSchema = new mongoose.Schema({
+    projectCode: String,
+    projectName: String,
+    projectType: String,
+    agencyCode: String,
+    agencyName: String,
+    approval: Boolean,
+    resourceApproval: Boolean,
+    fundApproval: Boolean,
+    closed: Boolean,
+    facultyID: String,
+    organizationType: String,
+    staff: [String],
+    noOfStaff: Number,
+    sanctionFund: Number,
+    startDate: Date,
+    endDate: Date,
+    status: Number,
+    description: String,
+    sanctionLetter: String, //shld be file
+    announcements: [String],
+    staffRecruitment: [{}],
+    items: [],
+  });
+  
+  export const Project = mongoose.model("Project", projectsSchema);
+  
+  //facultySchema
+  const facultySchema = new mongoose.Schema({
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    password: String,
+    userCode: String,
+    principalInvestigatorCode: String,
+    details: {
+      Department: String,
+      Designation: String,
+      Email: String,
+      ContactNumber: String,
+      DateOfJoining: Date,
+      Qualifications: String,
+      DoB: Date,
+      Address: String,
+      Gender: String,
+      Signature: [
+        {
+          fileName: String,
+          filePath: String,
+          fileType: String,
+          fileSize: String,
+        },
+      ],
+    },
+    projects: [String],
+    sanctionLetter: String, //shld be file
+  });
+  export const Faculty = mongoose.model("Faculty", facultySchema);
+  
+  
+  
+  //admin Schema
+  const adminSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    password: String,
+    projects: {
+      approve: [String],
+    },
+    faculty: [String],
+    staff: [String],
+    recruitment: {
+      project: String,
+      numberOfStaff: Number,
+      salary: Number,
+      startDate: Date,
+      endDate: Date,
+      reasonForRecruitment: String,
+      approve: Boolean,
+    },
+    details: {
+      Department: String,
+      Designation: String,
+      Email: String,
+      ContactNumber: String,
+      DateOfJoining: Date,
+      Qualifications: String,
+      DoB: Date,
+      Address: String,
+      Gender: String,
+    },
+  });
+  
+  export const Admin = mongoose.model("Admin", adminSchema);
+  
+  //staffSchema
+  const staffSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    password: String,
+    staffCode: String,
+    details: {
+      Department: String,
+      Designation: String,
+      Email: String,
+      ContactNumber: String,
+      DateOfJoining: Date,
+      Qualifications: String,
+      DoB: Date,
+      Address: String,
+    },
+    projects: [
+      {
+        projectID: String,
+        Salary: Number,
+        StartDate: Date,
+        EndDate: Date,
+      },
+    ],
+  });
+  
+  export const Staff = mongoose.model("Staff", staffSchema);
+  
+  
+  
+  //announcement Schema
+  const announcementSchema = new mongoose.Schema({
+    projectName: String,
+    projectID: String,
+    projectType: String,
+    salaryDetails: Number,
+    openPositions: String,
+    requiredQualifications: String,
+    startDate: Date,
+    endDate: Date,
+    active: Boolean,
+    description: String,
+    facultyName: String,
+    Applications: [String],
+  });
+  
+  export const Announcement = mongoose.model("Announcement", announcementSchema);
+  
+  //Recruitment details schema
+  const recruitmentRequestSchema = new mongoose.Schema({
+    projectID: String,
+    recruitmentType: String,
+    numberOfStaff: Number,
+    salaryDetails: Number,
+    startDate: Date,
+    endDate: Date,
+    active: Boolean,
+    description: String,
+    approval: Boolean,
+    projectName: String,
+    facultyID: String,
+    status: Number,
+  });
+  
+  export const RecruitmentRequest = mongoose.model(
+    "RecruitmentRequest",
+    recruitmentRequestSchema
+  );
+  
+  //fund extension data schema
+  const fundsRequestSchema = new mongoose.Schema({
+    projectID: String,
+    projectType: String,
+    prevSanctionValue: Number,
+    extendSanctionValue: Number,
+    active: Boolean,
+    descriptionBox: String,
+    approval: Boolean,
+    projectName: String,
+    facultyID: String,
+    status: Number,
+  });
+  
+  export const FundsRequest = mongoose.model("FundsRequest", fundsRequestSchema);
+  
+  //duration extension schema
+  const durationExtensionSchema = new mongoose.Schema({
+    projectID: String,
+    projectType: String,
+    numberOfStaff: Number,
+    prevDate: Date,
+    newDate: Date,
+    active: Boolean,
+    description: String,
+    approval: Boolean,
+    projectName: String,
+    descriptionBox: String,
+    facultyID: String,
+    status: Number,
+  });
+  
+  export const DurationExtension = mongoose.model(
+    "DurationExtension",
+    durationExtensionSchema
+  );
+  
+  //ident details schema
+  const indentSchema = new mongoose.Schema({
+    projectCode: String,
+    itemName: String,
+    cost: Number,
+    retailerName: String,
+    description: String,
+  });
+  
+  export const Indent = mongoose.model("Indent", indentSchema);
+  
+  //Test code for saving files
+  
+  // const scannedSignaturesSchema = new mongoose.Schema({
+  //   fileName: String,
+  //   filePath: String,
+  //   fileType: String,
+  //   fileSize: String,
+  // });
+  
+  // const ScannedSignatures = mongoose.model(
+  //   "ScannedSignatures",
+  //   scannedSignaturesSchema
+  // );
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { generateAgencyCode, projectCode } from "./Modules/backendModules.js";
+import { Project, Faculty, Staff, Admin, Announcement, RecruitmentRequest, FundsRequest, Indent, DurationExtension } from "./Modules/Schema.js";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -31,70 +32,9 @@ conn.on("connected", function () {
   console.log("database is connected successfully!");
 });
 
-//-----------------------------------------------------------------------------
-//SCHEMAS
-//-----------------------------------------------------------------------------
-
-const projectsSchema = new mongoose.Schema({
-  projectCode: String,
-  projectName: String,
-  projectType: String,
-  agencyCode: String,
-  agencyName: String,
-  approval: Boolean,
-  resourceApproval: Boolean,
-  fundApproval: Boolean,
-  closed: Boolean,
-  facultyID: String,
-  organizationType: String,
-  staff: [String],
-  noOfStaff: Number,
-  sanctionFund: Number,
-  startDate: Date,
-  endDate: Date,
-  status: Number,
-  description: String,
-  sanctionLetter: String, //shld be file
-  announcements: [String],
-  staffRecruitment: [{}],
-  items: [],
-});
-
-const Project = mongoose.model("Project", projectsSchema);
-
-//facultySchema
-const facultySchema = new mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  password: String,
-  userCode: String,
-  principalInvestigatorCode: String,
-  details: {
-    Department: String,
-    Designation: String,
-    Email: String,
-    ContactNumber: String,
-    DateOfJoining: Date,
-    Qualifications: String,
-    DoB: Date,
-    Address: String,
-    Gender: String,
-    Signature: [
-      {
-        fileName: String,
-        filePath: String,
-        fileType: String,
-        fileSize: String,
-      },
-    ],
-  },
-  projects: [String],
-  sanctionLetter: String, //shld be file
-});
-const Faculty = mongoose.model("Faculty", facultySchema);
+//--------------------------------------------------------------------------------------------
+//Dummy users
+//--------------------------------------------------------------------------------------------
 
 //dummy faculty
 
@@ -119,43 +59,6 @@ var newFac = new Faculty({
 newFac.save();
 //console.log(Faculty.findOne({ 'username': 'faculty' }))
 
-//admin Schema
-const adminSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  password: String,
-  projects: {
-    approve: [String],
-  },
-  faculty: [String],
-  staff: [String],
-  recruitment: {
-    project: String,
-    numberOfStaff: Number,
-    salary: Number,
-    startDate: Date,
-    endDate: Date,
-    reasonForRecruitment: String,
-    approve: Boolean,
-  },
-  details: {
-    Department: String,
-    Designation: String,
-    Email: String,
-    ContactNumber: String,
-    DateOfJoining: Date,
-    Qualifications: String,
-    DoB: Date,
-    Address: String,
-    Gender: String,
-  },
-});
-
-const Admin = mongoose.model("Admin", adminSchema);
-
 //dummy Admin
 let newAdmin = new Admin({
   username: "admin",
@@ -179,37 +82,6 @@ let newAdmin = new Admin({
 });
 newAdmin.save();
 
-//staffSchema
-const staffSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  password: String,
-  staffCode: String,
-  details: {
-    Department: String,
-    Designation: String,
-    Email: String,
-    ContactNumber: String,
-    DateOfJoining: Date,
-    Qualifications: String,
-    DoB: Date,
-    Address: String,
-  },
-  projects: [
-    {
-      projectID: String,
-      Salary: Number,
-      StartDate: Date,
-      EndDate: Date,
-    },
-  ],
-});
-
-const Staff = mongoose.model("Staff", staffSchema);
-
 //dummy staff object
 let newStaff = new Staff({
   username: "student",
@@ -228,107 +100,6 @@ let newStaff = new Staff({
   projects: [],
 });
 newStaff.save();
-
-//announcement Schema
-const announcementSchema = new mongoose.Schema({
-  projectName: String,
-  projectID: String,
-  projectType: String,
-  salaryDetails: Number,
-  openPositions: String,
-  requiredQualifications: String,
-  startDate: Date,
-  endDate: Date,
-  active: Boolean,
-  description: String,
-  facultyName: String,
-  Applications: [String],
-});
-
-const Announcement = mongoose.model("Announcement", announcementSchema);
-
-//Recruitment details schema
-const recruitmentRequestSchema = new mongoose.Schema({
-  projectID: String,
-  recruitmentType: String,
-  numberOfStaff: Number,
-  salaryDetails: Number,
-  startDate: Date,
-  endDate: Date,
-  active: Boolean,
-  description: String,
-  approval: Boolean,
-  projectName: String,
-  facultyID: String,
-  status: Number,
-});
-
-const RecruitmentRequest = mongoose.model(
-  "RecruitmentRequest",
-  recruitmentRequestSchema
-);
-
-//fund extension data schema
-const fundsRequestSchema = new mongoose.Schema({
-  projectID: String,
-  projectType: String,
-  prevSanctionValue: Number,
-  extendSanctionValue: Number,
-  active: Boolean,
-  descriptionBox: String,
-  approval: Boolean,
-  projectName: String,
-  facultyID: String,
-  status: Number,
-});
-
-const FundsRequest = mongoose.model("FundsRequest", fundsRequestSchema);
-
-//duration extension schema
-const durationExtensionSchema = new mongoose.Schema({
-  projectID: String,
-  projectType: String,
-  numberOfStaff: Number,
-  prevDate: Date,
-  newDate: Date,
-  active: Boolean,
-  description: String,
-  approval: Boolean,
-  projectName: String,
-  descriptionBox: String,
-  facultyID: String,
-  status: Number,
-});
-
-const DurationExtension = mongoose.model(
-  "DurationExtension",
-  durationExtensionSchema
-);
-
-//ident details schema
-const indentSchema = new mongoose.Schema({
-  projectCode: String,
-  itemName: String,
-  cost: Number,
-  retailerName: String,
-  description: String,
-});
-
-const Indent = mongoose.model("Indent", indentSchema);
-
-//Test code for saving files
-
-// const scannedSignaturesSchema = new mongoose.Schema({
-//   fileName: String,
-//   filePath: String,
-//   fileType: String,
-//   fileSize: String,
-// });
-
-// const ScannedSignatures = mongoose.model(
-//   "ScannedSignatures",
-//   scannedSignaturesSchema
-// );
 
 //--------------------------------------------------------------------------------------------
 //Login Authentication
